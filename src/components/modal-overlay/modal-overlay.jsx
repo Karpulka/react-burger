8d4ee85abbdc0ba9d5f6b3581a68fd39ad09f4e7
Alyona Karpova <karpova.dev@yandex.ru>
2022-11-05 17:31:37 +0300

    fix overlayModal & package.json

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import styles from './modal-overlay.module.css';
 
-const modalOverlayRoot = document.getElementById('modal-overlay');
-
 function ModalOverlay(props) {
-  return ReactDOM.createPortal(
-    <div className={styles['modal-overlay']} onClick={props.onClose}></div>,
-    modalOverlayRoot
-  );
+  return <div className={styles['modal-overlay']} onClick={props.onClose}></div>;
 }
 
 ModalOverlay.propTypes = {
